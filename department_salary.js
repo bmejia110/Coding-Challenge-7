e1e650c030413c7cfe9aead80ce00b1db9d2a524
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-04 21:47:00 -0400

    Initialize company structure with departments and employees

---
+++ department_salary.js
@@ -0,0 +1,62 @@
+const company = {
+    departments: [
+    {
+        departmentName: "Sales",
+        employees: [
+            {
+                name: "myles",
+                salary: 65000,
+                subordinates: [
+                    {
+                        name:"tyler",
+                        salary: 56000,
+                        subordinates: [
+                            {
+                                name: "nikki",
+                                salary: 48000,
+                                subordinates: []
+                            },
+                            {
+                                name: "david",
+                                salary: 45000,
+                                subordinates:[]
+                            }
+                        ]
+                    },
+                    {
+                        name:  "bri",
+                        salary:80000,
+                        subordinates: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        departmentName: "HR",
+        employees: [
+            {
+                name: "lorenzo",
+                salary: 76000,
+                subordinates: [
+                    { 
+                        name: "chaemin",
+                        salary: 65000,
+                        subordinates: []
+                    },
+                    {
+                        name: "kristian",
+                        salary: 63000,
+                        subordinates: []
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+                            
+                            
+
+            
+};
